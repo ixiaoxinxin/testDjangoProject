da35b61f682c4fd8e1cfd07dcecb34bb1b6ff1e8
ixiaoxinxin <295563386@qq.com>
2016-07-11 22:04:56 +0800

    python标准库中的 unittest 模块

--- src/testDjangoProject/functional_tests.py
+++ src/testDjangoProject/functional_tests.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 #引用标准的 unittest 模块
+import unittest
 
 
-
-browser = webdriver.Firefox()
-browser.get('http://localhost:8080')
-
-assert 'To-do' in browser.title()
-
-browser.quit()
+class NewVistorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser=webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        
+        self.browser.get('http://localhost:8080')
+        #网页的标题和头部都包含 todo
+        self.assertIn('To-do', self.browser.title())
+        # 生成制定的错误消息提醒测试结束
+        self.fail('finished the test!')
+    
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
